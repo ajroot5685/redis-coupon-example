b8b0ed445a65407edb862d35b80ca854455642db
ajroot5685 <anjeong8577@naver.com>
2025-04-16 14:18:18 +0900

    initial commit

---
+++ src/main/java/com/example/redis_coupon_example/RedisCouponExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.redis_coupon_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCouponExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisCouponExampleApplication.class, args);
+	}
+
+}
