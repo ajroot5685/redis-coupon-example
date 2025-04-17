d288b80fabf290cd4a287e76395a53ddd9163916
ajroot5685 <anjeong8577@naver.com>
2025-04-17 17:02:46 +0900

    refactor: API 수정 및 dto 레코드화

--- src/main/java/com/example/redis_coupon_example/dto/response/CouponIssueResultDto.java
+++ src/main/java/com/example/redis_coupon_example/dto/response/CouponIssueResultDto.java
@@ -1,12 +1,7 @@
 package com.example.redis_coupon_example.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class CouponIssueResultDto {
-
-    private boolean success;
-    private String couponName;
+public record CouponIssueResultDto(
+        boolean success,
+        String couponName
+) {
 }
